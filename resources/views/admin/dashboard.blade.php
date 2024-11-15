2ca52b801eec0104a8263f9db50a5dead002b2d4
Delvina <vinadelvina2001@gmail.com>
2024-11-15 20:03:16 +0700

    admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,104 @@
+<x-layout-admin>
+    <section class="dashboard custom-container nav-margin">
+        <div class="dashboard-header">
+            <p class="fs-2 text-light fw-bold">Dashboard</p>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Recipient's username"
+                    aria-label="Recipient's username" aria-describedby="button-addon2">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-search"></i>
+                </button>
+            </div>
+        </div>
+        <div class="dashboard-content">
+            <table class="table table-dark table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Message</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone Number</th>
+                        <th scope="col">Created By</th>
+                        <th scope="col">Updated At</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Hotel</td>
+                        <td>I need Something</td>
+                        <td>Mark203@Gmail.com</td>
+                        <td>+62 815-753-6753</td>
+                        <td>2024-11-12</td>
+                        <td>2024-11-13</td>
+                        <td class="d-flex custom-gap">
+                            <p>
+                                <a href="#"
+                                    class="link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">
+                                    Edit
+                                </a>
+                            </p>
+                            <p>
+                                <a href="#"
+                                    class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+                                    Delete
+                                </a>
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Hotel</td>
+                        <td>I need Something</td>
+                        <td>Mark203@Gmail.com</td>
+                        <td>+62 815-753-6753</td>
+                        <td>2024-11-12</td>
+                        <td>2024-11-13</td>
+                        <td class="d-flex custom-gap">
+                            <p>
+                                <a href="#"
+                                    class="link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">
+                                    Edit
+                                </a>
+                            </p>
+                            <p>
+                                <a href="#"
+                                    class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+                                    Delete
+                                </a>
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Hotel</td>
+                        <td>I need Something</td>
+                        <td>Mark203@Gmail.com</td>
+                        <td>+62 815-753-6753</td>
+                        <td>2024-11-12</td>
+                        <td>2024-11-13</td>
+                        <td class="d-flex custom-gap">
+                            <p>
+                                <a href="#"
+                                    class="link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">
+                                    Edit
+                                </a>
+                            </p>
+                            <p>
+                                <a href="#"
+                                    class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+                                    Delete
+                                </a>
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+</x-layout-admin>
